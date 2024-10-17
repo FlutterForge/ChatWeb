ee3c8a13742287374f2913194b948a7a68394ba2
Jamol Kholmirzayev <93211938+MrJamol@users.noreply.github.com>
2024-10-17 18:16:26 +0500

    number of FakeUsers are 35, now

--- lib/src/features/home/presentation/widget/fake_user_base.dart
+++ lib/src/features/home/presentation/widget/fake_user_base.dart
@@ -50,5 +50,130 @@ class FakeUserBase {
       'assets/images/person10.png',
       'DevOps Engineer',
     ],
+    'Ethan Moore': [
+      'Online',
+      'assets/images/person11.jpg',
+      'Network Engineer',
+    ],
+    'Alexander King': [
+      'Offline',
+      'assets/images/person12.jpg',
+      'Game Developer',
+    ],
+    'Jacob White': [
+      'Online',
+      'assets/images/person13.jpg',
+      'Blockchain Developer',
+    ],
+    'Lucas Walker': [
+      'Offline',
+      'assets/images/person14.jpg',
+      'UI/UX Designer',
+    ],
+    'Henry Hall': [
+      'Online',
+      'assets/images/person15.jpg',
+      'Systems Administrator',
+    ],
+    'Sebastian Allen': [
+      'Offline',
+      'assets/images/person16.jpg',
+      'SEO Specialist',
+    ],
+    'Jackson Young': [
+      'Online',
+      'assets/images/person17.jpg',
+      'Software Engineer',
+    ],
+    'Owen Martinez': [
+      'Offline',
+      'assets/images/person18.jpg',
+      'Machine Learning Engineer',
+    ],
+    'Nathan Hernandez': [
+      'Online',
+      'assets/images/person19.jpg',
+      'Web Developer',
+    ],
+    'Logan Lewis': [
+      'Offline',
+      'assets/images/person20.jpg',
+      'Security Analyst',
+    ],
+    'Mason Clark': [
+      'Online',
+      'assets/images/person21.jpg',
+      'Cloud Architect',
+    ],
+    'Carter Lee': [
+      'Offline',
+      'assets/images/person22.jpg',
+      'Mobile Game Developer',
+    ],
+    'Samuel Scott': [
+      'Online',
+      'assets/images/person23.jpg',
+      'IT Support Specialist',
+    ],
+    'Dylan Adams': [
+      'Offline',
+      'assets/images/person24.jpg',
+      'Data Engineer',
+    ],
+    'Noah Baker': [
+      'Online',
+      'assets/images/person25.jpg',
+      'Embedded Systems Engineer',
+    ],
+    'Isaac Perez': [
+      'Offline',
+      'assets/images/person26.jpg',
+      'Technical Writer',
+    ],
+    'Caleb Roberts': [
+      'Online',
+      'assets/images/person27.jpg',
+      'Database Administrator',
+    ],
+    'Ryan Turner': [
+      'Offline',
+      'assets/images/person28.jpg',
+      'Cloud Consultant',
+    ],
+    'Aiden Phillips': [
+      'Online',
+      'assets/images/person29.jpg',
+      'Front-End Developer',
+    ],
+    'Grayson Campbell': [
+      'Offline',
+      'assets/images/person30.jpg',
+      'Network Security Analyst',
+    ],
+    'Joshua Parker': [
+      'Online',
+      'assets/images/person31.jpg',
+      'DevOps Consultant',
+    ],
+    'Luke Evans': [
+      'Offline',
+      'assets/images/person32.jpg',
+      'Game Designer',
+    ],
+    'Matthew Edwards': [
+      'Online',
+      'assets/images/person33.jpg',
+      'IT Project Manager',
+    ],
+    'Isaiah Collins': [
+      'Offline',
+      'assets/images/person34.jpg',
+      'Digital Marketing Specialist',
+    ],
+    'Levi Stewart': [
+      'Online',
+      'assets/images/person35.jpg',
+      'Cybersecurity Analyst',
+    ],
   };
 }
